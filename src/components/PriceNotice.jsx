431a22cb46433f3f9c709ffc575582c99934880c
rayan2228 <rhrayan2228@gmail.com>
2024-03-20 12:16:03 +0600

    issue fixed 1

--- src/components/PriceNotice.jsx
+++ src/components/PriceNotice.jsx
@@ -8,13 +8,12 @@ const PriceNotice = () => {
     <section className="bg-[#FFF9DE] py-[100px] px-10 xl:px-0">
       <Container>
         <Flex className={"xl:items-center flex-col xl:flex-row gap-y-6"}>
-          <h2 className="font-banglaSemiBold text-3xl sm:text-4xl md:text-heading xl:w-[1092px] md:!leading-[70px] ">
+          <h2 className="font-banglaSemiBold text-3xl sm:text-4xl md:text-heading xl:w-[1092px] md:!leading-[70px] gap-y-10 ">
             ১ লাখ টাকা নগদ পুরষ্কার পাবে বিজয়ী শিক্ষার্থী, তাহলে দেরি না করে
             রেজিস্ট্রেশন করুন এখনই
           </h2>
-          <div>
-            <RegisterBtn />
-          </div>
+
+          <RegisterBtn />
         </Flex>
       </Container>
     </section>
--- src/components/layouts/RegisterBtn.jsx
+++ src/components/layouts/RegisterBtn.jsx
@@ -2,7 +2,7 @@ const RegisterBtn = ({ className }) => {
   return (
     <a
       href=""
-      className={`bg-[#423AD8] px-[50px] py-[30px] rounded-[45px] text-white text-xl font-semibold border-none  ${className} font-inter no-underline `}
+      className={`bg-[#423AD8] px-[50px] py-[30px] rounded-[45px] text-white text-xl font-semibold border-none  ${className} font-inter no-underline text-center `}
       style={{ boxShadow: "10px 16px 35.5px 0px rgba(66,58,216,0.3)" }}
     >
       Register Now
